73488de28f4279c76dec9a9573fa21e6713035ba
Welington Gulinelli Costa <welingtongulinelli69@gmail.com>
2024-10-28 13:16:47 -0300

    mudanca do button add chapas

--- index.js
+++ index.js
@@ -3,14 +3,26 @@ const app = express();
 const hand = require("express-handlebars");
 const Services = require("./services/services");
 const routes = require("./routes/routes");
+const path = require("path");
 
+// Configurar Handlebars como o mecanismo de visualização
 app.engine("handlebars", hand.engine());
 app.set("view engine", "handlebars");
+app.set("views", path.join(__dirname, "views"));
 
-app.use(express.urlencoded({extended: true,}));
+// Configurar a pasta 'public' para arquivos estáticos (CSS, imagens, etc.)
+// app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static("public"));
+
+
+// Configurar middlewares para o parsing de dados
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Configurar as rotas
 app.use("/", routes);
 
-
-app.listen(2000);
+// Iniciar o servidor na porta 2000
+app.listen(2000, () => {
+    console.log("Servidor rodando na porta 2000");
+});
